4e0901b9241387c639ecae326848bea23b272f7c
Kipl0 <maalmaja@gmail.com>
2023-02-26 20:38:44 +0100

    made a stable version

--- app.py
+++ app.py
@@ -15,23 +15,15 @@ def dict_factory(cursor, row):
 
 # This data will come from the database
 # For now, we just hard coded the data
-tweets = [
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"2.jpg", "fullname":"Joe Biden", "username":"joebiden","message":"I am THE president","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-]
+# tweets = [
+#   { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+#   { "verified":0, "image_name":"2.jpg", "fullname":"Joe Biden", "username":"joebiden","message":"I am THE president","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+#   { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+#   { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+#   { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+#   { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
+# ]
 
-my_tweets = [
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"2.jpg", "fullname":"Joe Biden", "username":"joebiden","message":"I am THE president","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":1, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-  { "verified":0, "image_name":"1.jpg", "fullname":"Santiago Donoso", "username":"santiagodonoso","message":"My first tweet","message_image":"1.png","total_messages":"1","total_retweets":"2","total_likes":"3","total_dislikes":"4",},
-]
 
 trends = [
   {"title":"LGBTQ", "total_hash":58.5},
@@ -40,18 +32,6 @@ trends = [
   {"title":"Politics", "total_hash":17.3},
   {"title":"Bilka", "total_hash":10.8}
 ]
-
-# follows = [
-#   {"profile_pic":"1.jpg","fullname":"Elon Musk", "username":"elonmusk", "route":"/elonmusk"},
-#   {"profile_pic":"2.jpg","fullname":"Joe Biden", "username":"katyperry", "route":"/katyperry"},
-#   {"profile_pic":"3.jpg","fullname":"Shakira", "username":"rihanna", "route":"/rihanna"}
-# ]
-
-# follows = [
-  # {"id":"uuid", user_fk:uuid4, route:varchar}
-  # {id:uuid4, user_fk:uuid4, route:varchar}
-# ]
-
 
 
 profiles = [
@@ -84,14 +64,14 @@ def _(filename):
   return static_file(filename, root="./images")
 
 ##############################
-@get("/thumbnails/<filename:re:.*\.png>")
+@get("/avatar/<filename:re:.*\.png>")
 def _(filename):
-  return static_file(filename, root="./thumbnails")
+  return static_file(filename, root="./avatar")
 
 ##############################
-@get("/thumbnails/<filename:re:.*\.jpg>")
+@get("/avatar/<filename:re:.*\.jpg>")
 def _(filename):
-  return static_file(filename, root="./thumbnails")
+  return static_file(filename, root="./avatar")
 
 
 
@@ -111,6 +91,8 @@ def render_index():
     db = sqlite3.connect("twitter.db")
     db.row_factory = dict_factory
     user_suggested_follows = db.execute("SELECT * FROM users WHERE username!=?",("majs503",)).fetchall()
+    # tweets = db.execute("SELECT * FROM tweets")
+    tweets = db.execute("SELECT * FROM tweets,users WHERE tweets.user_fk = users.id")
     return template("index", title="Twitter", tweets=tweets, trends=trends, user_suggested_follows=user_suggested_follows)
     
   except Exception as ex:
@@ -142,7 +124,7 @@ def _(username):
     #with that id look up the respectives tweets
     #pass the tweets to the view. Template it
     tweets = db.execute("SELECT * FROM tweets WHERE user_fk=?",(user_id,)).fetchall()
-    return template("profile", user=user, tweets=tweets, my_tweets=my_tweets, trends=trends, user_suggested_follows=user_suggested_follows)
+    return template("profile", user=user, tweets=tweets, trends=trends, user_suggested_follows=user_suggested_follows)
 
   except Exception as ex:
     print(ex)
--- data/create_table_users.sql
+++ data/create_table_users.sql
@@ -3,20 +3,21 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
   id                TEXT,
   username          TEXT,
-  name              TEXT,
+  first_name        TEXT,
   last_name         TEXT,
   total_followers   TEXT,
   total_following   TEXT,
   total_tweets      TEXT,
   avatar            TEXT,
   banner            TEXT,
+  verified          TEXT,
   PRIMARY KEY(id)
   ) WITHOUT ROWID;
 
-INSERT INTO users VALUES("51602a9f7d82472b90ed1091248f6cb1","elonmusk","Elon", "Musk", "128900000", "177", "22700","51602a9f7d82472b90ed1091248f6cb1.jpg", "d8109732ae4f472aa4fe935b1e25c1aa.jpg");
-INSERT INTO users VALUES("6268331d012247539998d7664bd05cc1","katyperry","Katy", "Perry", "53700000", "235", "7999", "6268331d012247539998d7664bd05cc1.jpg", "494e6a7fdadb4b3cae58d37a4fad879c.jpg");
-INSERT INTO users VALUES("a22da1effb3d4f03a0f77f9aa8320203","Rihanna","Rihanna", "", "107000000", "980", "10600", "a22da1effb3d4f03a0f77f9aa8320203.jpg", "e130fd8b81d049a1b2fafbca9c5a15e3.jpg");
-INSERT INTO users VALUES("07578f6c49d84b7c94ce80e96c64ccc0","majs503","Maja", "Ibs Larsen", "0", "0", "0", "07578f6c49d84b7c94ce80e96c64ccc0.jpg", "dd5582fff3ca4f7f9f97a911f3e77b22.jpg");
+INSERT INTO users VALUES("51602a9f7d82472b90ed1091248f6cb1","elonmusk","Elon", "Musk", "128900000", "177", "22700","51602a9f7d82472b90ed1091248f6cb1.jpg", "d8109732ae4f472aa4fe935b1e25c1aa.jpg", "1");
+INSERT INTO users VALUES("6268331d012247539998d7664bd05cc1","katyperry","Katy", "Perry", "53700000", "235", "7999", "6268331d012247539998d7664bd05cc1.jpg", "494e6a7fdadb4b3cae58d37a4fad879c.jpg", "1");
+INSERT INTO users VALUES("a22da1effb3d4f03a0f77f9aa8320203","Rihanna","Rihanna", "", "107000000", "980", "10600", "a22da1effb3d4f03a0f77f9aa8320203.jpg", "e130fd8b81d049a1b2fafbca9c5a15e3.jpg", "1");
+INSERT INTO users VALUES("07578f6c49d84b7c94ce80e96c64ccc0","majs503","Maja", "Ibs Larsen", "0", "0", "0", "07578f6c49d84b7c94ce80e96c64ccc0.jpg", "dd5582fff3ca4f7f9f97a911f3e77b22.jpg", "0");
 
 
 
