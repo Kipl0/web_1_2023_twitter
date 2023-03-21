f01d7f220f3b3ce9449c0377084291019af07d3d
Maja Larsen <maalmaja@gmail.com>
2023-03-21 17:14:10 +0100

    seperate banners from avatars

--- main.sql
+++ main.sql
@@ -28,9 +28,9 @@ CREATE TABLE users(
 ) WITHOUT ROWID;
 
 INSERT INTO users VALUES("51602a9f7d82472b90ed1091248f6cb1","HarryMemes","HarryMemes@gmail.com","123","be3a21b8186f43bf82483074d06f4ab4","Harry Potter", "Memes", "51602a9f7d82472b90ed1091248f6cb1.jpeg", "ad1bfe9ce6e44a009b57a1a183ccb202.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
-INSERT INTO users VALUES("a22da1effb3d4f03a0f77f9aa8320203","geekdaddy75","geekdaddy75@gmail.com","123","6162dc2c622546c79a526fe068ed6dfb","GeekDaddy", "Memes", "a22da1effb3d4f03a0f77f9aa8320203.jpeg", "e130fd8b81d049a1b2fafbca9c5a15e3.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
-INSERT INTO users VALUES("6268331d012247539998d7664bd05cc1","katyperry","Katy@gmail.com","123","d2799c780985435c8e731751b31162f4","Katy", "Perry", "6268331d012247539998d7664bd05cc1.jpeg", "494e6a7fdadb4b3cae58d37a4fad879c.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
-INSERT INTO users VALUES("07578f6c49d84b7c94ce80e96c64ccc0","majs503","maalmaja@gmail.com","123","e97db58a36ea498894f364f0c1c0c83e","Maja", "Larsen", "07578f6c49d84b7c94ce80e96c64ccc0.jpeg", "dd5582fff3ca4f7f9f97a911f3e77b22.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
+INSERT INTO users VALUES("a22da1effb3d4f03a0f77f9aa8320203","geekdaddy75","geekdaddy75@gmail.com","123","6162dc2c622546c79a526fe068ed6dfb","GeekDaddy", "Memes", "a22da1effb3d4f03a0f77f9aa8320203.jpg", "e130fd8b81d049a1b2fafbca9c5a15e3.png", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
+INSERT INTO users VALUES("6268331d012247539998d7664bd05cc1","katyperry","Katy@gmail.com","123","d2799c780985435c8e731751b31162f4","Katy", "Perry", "6268331d012247539998d7664bd05cc1.jpg", "494e6a7fdadb4b3cae58d37a4fad879c.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
+INSERT INTO users VALUES("07578f6c49d84b7c94ce80e96c64ccc0","majs503","maalmaja@gmail.com","123","e97db58a36ea498894f364f0c1c0c83e","Maja", "Larsen", "07578f6c49d84b7c94ce80e96c64ccc0.jpg", "dd5582fff3ca4f7f9f97a911f3e77b22.jpg", "1679402780","","","","1", "0", "0", "0","0","0","0","0");
 
 
 
