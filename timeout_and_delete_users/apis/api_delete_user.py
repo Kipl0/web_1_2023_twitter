e82b2cb1c18715d73f356516e804f06ce2ece252
Kipl0 <maalmaja@gmail.com>
2023-04-15 19:59:14 +0200

    add timeout and delete files from class

---
+++ timeout_and_delete_users/apis/api_delete_user.py
@@ -0,0 +1,31 @@
+from bottle import post, request, response, delete
+import sqlite3
+
+# @post("/api-delete-user") # godt for indlæring at kalde det sådan, men man ville ikke se det her i et firma. Vi bruger delete i stedet for post
+@delete("/user/<user_id>")
+def _(user_id):
+  try: 
+    # db = sqlite3.connect("company.db")
+    # user_id = request.forms.get("user_id")
+    # print(user_id)
+
+    # user_delete = db.execute("DELETE FROM users WHERE user_id=?", (user_id,)).rowcount
+    # if not user_delete : raise Exception(400, "User not found")
+    # db.commit()
+
+    # return {
+    #     "info" : "User deleted",
+    #     "user_id" : user_id
+    # }
+
+    return f"user deleted : {user_id}"
+
+  except Exception as ex: 
+    response.status = 400
+    return str(ex)
+
+  finally: 
+    if "db" in locals(): db.close()
+
+
+    
