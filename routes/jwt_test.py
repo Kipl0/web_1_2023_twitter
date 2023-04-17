9003a750c2a03a7b78dfac779110db2148ade00f
Kipl0 <maalmaja@gmail.com>
2023-04-17 12:32:12 +0200

    add jwt files

---
+++ routes/jwt_test.py
@@ -0,0 +1,22 @@
+from bottle import response, get, template
+import jwt
+import x
+
+@get("/jwt-test")
+def _():
+    the_jwt = jwt.encode({"name":"Maja","last_name":"Larsen"}, "the secret", algorithm="HS256")
+
+    try :
+        user_jwt = jwt.decode(the_jwt, "the secret", algorithms=["HS256"])
+        # response.set_cookie("jwt", user_jwt, secret=x.COOKIE_SECRET, httponly=True) #Udkommenteret fordi, jeg ikke skal have en cookie liggenede aktivt.
+        # print(user_jwt)
+
+        return template("jwt_test")
+    except Exception as e:
+            print("Sorry cannot verify jwt")
+
+    finally :
+        pass
+
+
+
